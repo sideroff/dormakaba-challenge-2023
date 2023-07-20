4cf47895b63608876400657be0eb5c179d146ac0
sideroff <ivan.sideroff@gmail.com>
2023-07-20 17:35:58 +0300

    UC-7: Fix Next.js LCP image warning

--- src/ui/layout/AppBarLogo.tsx
+++ src/ui/layout/AppBarLogo.tsx
@@ -6,7 +6,13 @@ export function AppBarLogo() {
   return (
     <Box sx={{ display: { xs: 'none', md: 'flex' }, mr: 2 }}>
       <Link href="/">
-        <Image src="/logo.png" alt="dormakaba logo" width="269" height="30" />
+        <Image
+          src="/logo.png"
+          priority
+          alt="dormakaba logo"
+          width="269"
+          height="30"
+        />
       </Link>
     </Box>
   );
