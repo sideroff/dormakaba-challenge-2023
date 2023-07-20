a3917dab5dea3097e836e1f2e59b1eaf697bb441
sideroff <ivan.sideroff@gmail.com>
2023-07-20 17:19:11 +0300

    UC-6: Fix ts errors

--- src/ui/components/DoorList.test.tsx
+++ src/ui/components/DoorList.test.tsx
@@ -9,6 +9,8 @@ const doors: Door[] = [
     id: '63f637c9f3c48a124616044b',
     name: 'Building Main Entrance',
     buildingName: 'Bahnhofstrasse 10A',
+    apartmentName: 'Apartment 1',
+    lastConnectionStatusUpdate: '2023-02-12T14:30:20.124',
     connectionType: 'wired',
     connectionStatus: 'offline',
   },
