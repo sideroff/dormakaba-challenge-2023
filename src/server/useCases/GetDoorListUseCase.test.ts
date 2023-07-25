c71f8117ae388200c0670c7d4c09a628871bce77
sideroff <ivan.sideroff@gmail.com>
2023-07-25 14:24:11 +0300

    UC-8: Add additional unit tests to cover tasks

--- src/server/useCases/GetDoorListUseCase.test.ts
+++ src/server/useCases/GetDoorListUseCase.test.ts
@@ -3,6 +3,7 @@ import { HttpError } from 'http-errors';
 import { DoorRepository } from '@/server/repositories/DoorRepository';
 import { BuildingRepository } from '@/server/repositories/BuildingRepository';
 import { GetDoorListUseCase } from './GetDoorListUseCase';
+import { ApartmentRepository } from '../repositories/ApartmentRepository';
 
 describe('GetDoorListUseCase', () => {
   let getDoorListUseCase: GetDoorListUseCase;
@@ -21,10 +22,15 @@ describe('GetDoorListUseCase', () => {
       .spyOn(BuildingRepository.prototype, 'getAllBuildings')
       .mockImplementation(() => Promise.resolve([]));
 
+    const getAllApartmentsSpy = jest
+      .spyOn(ApartmentRepository.prototype, 'getAllApartments')
+      .mockImplementation(() => Promise.resolve([]));
+
     await getDoorListUseCase.execute();
 
     expect(getAllDoorsSpy).toHaveBeenCalledTimes(1);
     expect(getAllBuildingsSpy).toHaveBeenCalledTimes(1);
+    expect(getAllApartmentsSpy).toHaveBeenCalledTimes(1);
   });
 
   it('should throw if getAllBuildings request fails', async () => {
@@ -35,8 +41,11 @@ describe('GetDoorListUseCase', () => {
     const getAllBuildingsSpy = jest
       .spyOn(BuildingRepository.prototype, 'getAllBuildings')
       .mockImplementation(() => {
-        throw new Error();
+        return Promise.reject(new Error());
       });
+    const getAllApartmentsSpy = jest
+      .spyOn(ApartmentRepository.prototype, 'getAllApartments')
+      .mockImplementation(() => Promise.resolve([]));
 
     try {
       await getDoorListUseCase.execute();
@@ -46,7 +55,8 @@ describe('GetDoorListUseCase', () => {
 
     expect(getAllDoorsSpy).toHaveBeenCalledTimes(1);
     expect(getAllBuildingsSpy).toHaveBeenCalledTimes(1);
+    expect(getAllApartmentsSpy).toHaveBeenCalledTimes(1);
 
-    expect.assertions(3);
+    expect.assertions(4);
   });
 });
